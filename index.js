cc941ac66171add533eb307bf4386523ccd8c9d5
sabbirerror44 <sabbirerror44@gmailcom>
2020-08-05 23:22:15 +0600

    Variable name organised and some funcitonality added

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-    //Handling Input taken from user to match
-    function insert(num){
-        document.getElementById("inputPin").value += num;
+      //Handling Input taken from user to match
+      function insert(num){
+        document.getElementById("input-field").value += num;
         }
-    function clean(){
-        document.getElementById("inputPin").value = "";
+      function clean(){
+        document.getElementById("input-field").value = "";
         }
-    function backspace(){
-        document.getElementById("inputPin").value = document.getElementById("inputPin").value.slice(0,-1);
+      function backspace(){
+        document.getElementById("input-field").value = document.getElementById("input-field").value.slice(0,-1);
         }
 
-     //Random Pin generator
-      const PinGenerator = document.getElementById("pin-generator");
+      //Random Pin generator
+      const PinGenerator = document.getElementById("generate-pin");
       PinGenerator.addEventListener("click",function(){
-      const inputScreen = document.getElementById("randomNum");
-      const randomValue = inputScreen.value = Math.floor(1000+Math.random()*9000);
-    
-    //clearing the notification after new generate number
+      const randomPin= document.getElementById("random-pin");
+      const randomValue = randomPin.value = Math.floor(1000+Math.random()*9000);
+
+      //clearing the notification after new generate number
       let NotMatched = document.getElementById("NotMatched");
       NotMatched.style.display ="none";
       let Matched = document.getElementById("Matched");
@@ -23,22 +23,22 @@
 
       const submitBtn = document.getElementById("submitBtn");
       submitBtn.addEventListener("click",function(){
-      const inputPin = document.getElementById("inputPin").value;
-  
-    //Checking whether randomValue and Pin are equal or not 
-      if(randomValue == inputPin){
-       let Matched = document.getElementById("Matched");
+      const inputField = document.getElementById("input-field").value;
+
+      //Checking whether randomValue and Pin are equal or not 
+      if(randomValue == inputField){
+      let Matched = document.getElementById("Matched");
         Matched.style.display = "block";
         let NotMatched = document.getElementById("NotMatched");
         NotMatched.style.display ="none";
       }
 
-      if(randomValue != inputPin){
-       let NotMatched = document.getElementById("NotMatched");
-       NotMatched.style.display ="block";
-       let Matched = document.getElementById("Matched");
-       Matched.style.display = "none";
-   }
+      if(randomValue != inputField){
+      let NotMatched = document.getElementById("NotMatched");
+      NotMatched.style.display ="block";
+      let Matched = document.getElementById("Matched");
+      Matched.style.display = "none";
+      }
 
-  })  
-}) 
+      })  
+      }) 
